98992ba4e07cd8ee3cdadfc49e101e646da8c4fd
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-03-29 17:23:41 -0300

    Criado service get categoria e o model do type

---
+++ src/app/features/categories/service/category.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Categoria } from '../model/category.model';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  private apiUrl = environment.apiUrl + '/categories';
+  private readonly httpClient = inject(HttpClient);
+  // private apiUrl = 'http://localhost:3000/categories';
+  // private apiUrl = 'https://api.example.com/categories';
+
+  public categorias = signal<Categoria[]>([]);
+
+  public getCategories(): Observable<Categoria[]> {
+    return this.httpClient.get<Categoria[]>(this.apiUrl);
+  }
+}
