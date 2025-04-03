c1f67ba27b0262b79327f291ad32864545293f52
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-03 10:21:46 -0300

    criado os botões de categorias com cor e name

--- src/app/features/categories/components/colors-list/colors-list/colors-list.component.ts
+++ src/app/features/categories/components/colors-list/colors-list/colors-list.component.ts
@@ -1,18 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { CategoryService } from '../../../service/category.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-colors-list',
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [MatDividerModule, CommonModule, AsyncPipe, MatButtonModule],
   template: `
     <section class="flex flex-col gap-4 w-full">
       <mat-divider class="opacity-50 w-full"></mat-divider>
-      <span>Titulo colors...</span>
-      botoes categ
+      <div class="flex flex-wrap pl-1">
+        <div
+          class="flex flex-wrap items-center h-full
+          gap-4">
+          @for (categories of categorias$(); track categories.id) {
+            <button
+              [style.backgroundColor]="categories.color"
+              mat-flat-button
+              type="button">
+              {{ categories.name }}
+            </button>
+          }
+        </div>
+      </div>
     </section>
   `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColorsListComponent {}
+export class ColorsListComponent {
+  private readonly categoryService = inject(CategoryService);
+  public categorias$ = this.categoryService.categorias;
+}
